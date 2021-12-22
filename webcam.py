16aeec416cd78b62fea54ece47ff97c7f2b38c28
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-22 10:28:06 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -10,4 +10,4 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Classifying...",type(image))
