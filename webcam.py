a01c6386634ec25ee9193aeadcd33bcb8f692637
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-22 18:02:09 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,13 +1,14 @@
-# from webcam import webcam
-import streamlit as st
-from PIL import Image
+import cv2
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-### Excluding Imports ###
-st.title("Upload + Classification Example")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...",type(image))
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+        return img
+
+
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
