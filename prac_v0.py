e76a287259cc261a3994b8daeecf091f2f709a48
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-22 16:00:51 +0530

    Update prac_v0.py

--- prac_v0.py
+++ prac_v0.py
@@ -4,7 +4,7 @@ from PIL import Image
 #----
 st.title("--Image Visualize--")
 ip_image=st.file_uploader("--please upload an image--", type="jpg")
-if ip_image not None:
+if ip_image is not None:
   image = Image.open(ip_image)
 
   st.write("-your imput image-")
