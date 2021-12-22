d97a7be9ebb49be0ff1d3ac602b28aa80dbe78fb
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-22 18:21:06 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,14 +1,4 @@
-import cv2
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
-
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-        return img
-
-
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+webrtc_streamer(key="example")
