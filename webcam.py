0a11322a49c2103f52620d2248050d93bfc1cd33
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-28 11:05:48 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -5,9 +5,11 @@ st.title("Webcam Application")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 cam = cv2.VideoCapture(0)
-
+st.write("---captured images---")
 while run:
     ret, frame = cam.read()
+    st.write("---read images---")
+    
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
